b2cbba1852c2899ac8a1e5cc6f83d41c6576617e
sjk <sungntung@gmail.com>
2025-02-06 09:10:17 +0900

    chore: change base dir, use esm

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "/",
-  base: "/maimai-shiftpsh",
+  base: "/maimai/",
   build: {
     outDir: "dist/maimai/",
   },
